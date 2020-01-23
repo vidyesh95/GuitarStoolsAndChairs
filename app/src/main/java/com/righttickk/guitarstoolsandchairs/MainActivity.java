424ed90da6c7ab37263d5e272f1e8ca99ea4c42f
vidyesh95 <vidyesh95@gmail.com>
2020-01-23 12:08:32 +0530

    2ND CHANGE

--- app/src/main/java/com/righttickk/guitarstoolsandchairs/MainActivity.java
+++ app/src/main/java/com/righttickk/guitarstoolsandchairs/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    // Test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
