5255d77fe453ed4cdf0289084a2f9448ee38b41c
vidyesh95 <vidyesh95@gmail.com>
2020-02-04 22:32:45 +0530

    import update

--- app/src/main/java/com/righttickk/guitarstoolsandchairs/ui/home/HomeFragment.java
+++ app/src/main/java/com/righttickk/guitarstoolsandchairs/ui/home/HomeFragment.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.ItemTouchHelper;
